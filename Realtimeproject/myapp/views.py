47b8b970c8bcf5a2cf92fe30f7594bab59131bc2
shristiii <shristiadhikari000@gmail.com>
2022-03-15 18:44:28 +0545

    BLOG

--- Realtimeproject/myapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def login_page(request):
-    return render(request,"login.html")
-def register_page(request):
-    return render(request,"registration.html")
